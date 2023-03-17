a6f0453b9a26af5f709df08fd944ead866ddee07
canhsan <1979hasancan@gmail.com>
2023-03-17 14:41:44 +0300

    ikinci versiyon

---
+++ src/string_manipulation/ders_03.java
@@ -0,0 +1,37 @@
+package string_manipulation;
+
+import java.util.Scanner;
+
+public class ders_03 {
+    public static void main(String[] args) {
+       /* Kullanicidan isim ve soyismini ayri ayri alin.
+                - ismi daha uzun ise, isim ve soyismi ilk harf buyuk, kalanlar kucuk seklinde
+        yazdirin
+                - soyisim daha uzun ise ismi ilk harf buyuk digerleri kucuk, soyismi buyuk
+        harflerle yazdirin.
+        */
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lütfen isminizi giriniz");
+        String isim = scan.nextLine();
+
+        System.out.println("Lütfen Soy isminizi giriniz");
+        String soyisim = scan.nextLine();
+
+        if (isim.length() > soyisim.length()) {
+            System.out.println(isim.substring(0, 1).toUpperCase() + isim.substring(1) +
+                    " " + soyisim.substring(0, 1).toUpperCase() + soyisim.substring(1));
+
+        } else if (isim.length() < soyisim.length()) {
+            System.out.println(isim.substring(0, 1).toUpperCase() + isim.substring(1) +
+                    " " + soyisim.toUpperCase());
+
+        } else {
+            System.out.println(isim.toUpperCase() + " " + soyisim.toLowerCase());
+        }
+
+    }
+
+
+}
---
+++ src/string_manipulation/ders_05.java
@@ -0,0 +1,28 @@
+package string_manipulation;
+
+import java.util.Scanner;
+
+public class ders_05 {
+    public static void main(String[] args) {
+        /*
+        Kullanicidan alinan bir String alin, String’in uzunlugu cift sayi ise tam ortasina
+         :) ekleyin, String’in uzunlugu tek sayi ise ortadaki harfi silin ve yerine :(
+         yazdirin.
+         */
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lütfen bir cümle giriniz");
+        String input=scan.nextLine();
+        //int uzunluk=input.length();
+
+        if ((input.length()/2)%2==0){
+            //int mid=uzunluk/2;
+            input=input.substring(0,input.length()/2)+":)"+input.substring(input.length()/2);
+        }else {
+            //int mid=uzunluk/2;
+            input=input.substring(0,input.length()/2)+":("+input.substring(input.length()/2+1);
+        }
+        System.out.println("sonuç:"+ input);
+
+    }
+}
